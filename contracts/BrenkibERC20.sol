671a3bf443b77cc7df338ff3925f63be01973119
Bren Brenkib <kennybush96@gmail.com>
2024-07-31 10:09:54 +0200

    Testing deployment of ERC20 token on arb and sepolia

---
+++ contracts/BrenkibERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@layerzerolabs/oft-evm/contracts/OFT.sol";
+
+contract BrenkibERC20 is OFT {
+    constructor(
+        address _lzEndpoint,
+        address _delegate
+    ) OFT("BrenkibERC20", "BRN20", _lzEndpoint, _delegate) Ownable(_delegate) {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
